269889e471c8bd33a8a44824c91a01e10109cc42
uday873 <udaykiranpasuladi2004@gmail.com>
2025-11-04 00:26:26 +0530

    Initial commit - full Voxify AI project

---
+++ backend/voxify/Lib/site-packages/assemblyai/streaming/v3/__init__.py
@@ -0,0 +1,31 @@
+from .client import StreamingClient
+from .models import (
+    BeginEvent,
+    Encoding,
+    EventMessage,
+    SpeechModel,
+    StreamingClientOptions,
+    StreamingError,
+    StreamingEvents,
+    StreamingParameters,
+    StreamingSessionParameters,
+    TerminationEvent,
+    TurnEvent,
+    Word,
+)
+
+__all__ = [
+    "BeginEvent",
+    "Encoding",
+    "EventMessage",
+    "SpeechModel",
+    "StreamingClient",
+    "StreamingClientOptions",
+    "StreamingError",
+    "StreamingEvents",
+    "StreamingParameters",
+    "StreamingSessionParameters",
+    "TerminationEvent",
+    "TurnEvent",
+    "Word",
+]
